47f7ab2ec2e55c226cda76431c2cdfb80acaf481
anacpp <anacarlacpp@gmail.com>
2025-06-05 21:24:15 -0300

    add : music and sound effects

--- game_logic.py
+++ game_logic.py
@@ -2,6 +2,7 @@ import pgzrun
 from settings import *
 from player import Player
 from enemies import Enemy
+from pgzero.builtins import music
 
 WIDTH = TILE_SIZE * MAP_WIDTH
 HEIGHT = TILE_SIZE * MAP_HEIGHT
@@ -17,16 +18,22 @@ game_win = False
 game_state = "menu"
 background = Actor('background', (WIDTH // 2, HEIGHT // 2))
 current_music = None
+sound_on = True
 
 def play_game_music(track_name):
        global current_music
-       
+
        if current_music != track_name:
-        print(f"Tocando música: {track_name}") 
-        music.stop()
+        print(f"Tocando música: {track_name}")
+        music.set_volume(0.7)
         music.play(track_name)
-        music.set_volume(0.5)
         current_music = track_name
+
+def play_effect(track_name):
+       
+        music.set_volume(1.0)
+        music.play_once(track_name)
+
 
 def on_mouse_down(pos):
     global game_state, sound_on
@@ -62,11 +69,12 @@ def load_map():
             elif val == 'D':
                 door = Actor('door', pos)
 load_map()
+play_game_music('game_music')
 
 def draw():
     screen.clear()
     if game_state == "menu":
-        play_game_music("game_music")
+        
 
         background.draw()
         screen.draw.text('WHERE IS MY SNACK', center=(400, 80), fontsize=60, color="white")
@@ -80,8 +88,7 @@ def draw():
         screen.draw.text("Exit", center=exit_button.center, color="white")
 
     elif game_state == "playing":
-        play_game_music("game_music")
-
+        
         for wall in walls:
             wall.draw()
         for coin in coins:
@@ -111,16 +118,19 @@ def update():
 
     for coin in coins[:]: 
         if player.actor.colliderect(coin):
+            play_effect('points_sound')
             coins.remove(coin)
 
     for enemy in enemies:
         if player.actor.colliderect(enemy.actor):
+            play_effect('game_over')
             print("Game Over!")
             game_over = True
 
     if len(coins) == 0 and door:
         if player.actor.colliderect(door):
             print("You win!")
+            play_effect('win')
             game_win = True
 
 pgzrun.go()
